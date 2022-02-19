47407e248f0857f4f9c1a9e34066182fb4901677
rosh-1228 <sho.watanabe1228@gmail.com>
2022-02-19 21:35:19 +0900

    テスト: 日にちを配列で取得するコードがないので失敗するテストコード

--- 02.calendar/__test__/header.test.js
+++ 02.calendar/__test__/header.test.js
@@ -7,3 +7,11 @@ test('load this month this year', () => {
 test('load weekly', () => {
   expect(testCalender.showWeekly()).toBe('日 月 火 水 木 金 土')
 })
+
+test('current year and month', () => {
+  let date = new Date()
+  date.setMonth(date.getMonth() + 1)
+  date.setDate(0)
+  const calArray = Array.from(new Array(date.getDate())).map((v, i) => i + 1)
+  expect(testCalender.calArray).toBe(calArray)
+})
