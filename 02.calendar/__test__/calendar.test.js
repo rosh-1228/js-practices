c13b89163802205086316d321867c227a33d83f6
rosh-1228 <sho.watanabe1228@gmail.com>
2022-02-19 16:44:45 +0900

    テスト: 土曜日から日曜日まで表示するコードがないので失敗

--- 02.calendar/__test__/calendar.test.js
+++ 02.calendar/__test__/calendar.test.js
@@ -3,3 +3,9 @@ const calHeader = require('../lib/calendar')
 test('load this month this year', () => {
   expect(showHeader()).toBe('      2月 2022')
 })
+
+const weekly = require('../lib/calendar')
+
+test('load weekly', () => {
+  expect(showWeekly()).toBe(['日 月 火 水 木 金 土')
+})
