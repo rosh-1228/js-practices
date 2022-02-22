ab5d795dab68d7f7add6efece859391ccac05bc7
rosh-1228 <sho.watanabe1228@gmail.com>
2022-02-22 23:06:56 +0900

    ESLintでの修正

--- 01.fizzbuzz/fizz_buzz.js
+++ 01.fizzbuzz/fizz_buzz.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
-for(i = 1; i < 21; i++){
-  if(i % 5 == 0 && i % 3 == 0){
-    console.log('FizzBuzz');
-  }else if(i % 3 == 0){
-    console.log('Fizz');
-  }else if(i % 5 == 0){
-    console.log('Buzz');
-  }else{
-    console.log(i.toString());
+for (let i = 1; i < 21; i++) {
+  if (i % 5 === 0 && i % 3 === 0) {
+    console.log('FizzBuzz')
+  } else if (i % 3 === 0) {
+    console.log('Fizz')
+  } else if (i % 5 === 0) {
+    console.log('Buzz')
+  } else {
+    console.log(i)
   }
 }
