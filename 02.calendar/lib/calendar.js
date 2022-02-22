7be47586a12867af89fe0ea4554aee73996d82a5
rosh-1228 <sho.watanabe1228@gmail.com>
2022-02-22 13:53:43 +0900

    カレンダー機能を実装

--- 02.calendar/lib/calendar.js
+++ 02.calendar/lib/calendar.js
@@ -1,26 +1,51 @@
 #!/usr/bin/env node
 
-const weekly = ['日 ', '月 ', '火 ', '水 ', '木 ', '金 ', '土']
-
-let showHeader = () => {
-  let header = new Date()
-  return '      ' + (header.getMonth() + 1) + '月 ' + header.getFullYear()
+const main = () => {
+  const monthAndYear = require('minimist')(process.argv.slice(2))
+  const date = loadYearMonth(monthAndYear.m, monthAndYear.y)
+  console.log(`      ${(date.getMonth() + 1)}月 ${date.getFullYear()}`)
+  console.log('日 月 火 水 木 金 土')
+  console.log(outputCalendar(date))
 }
-exports.showHeader = showHeader
 
-
-let showWeekly = () => {
-  return weekly.join('')
+const loadYearMonth = (month = 0, year = 0) => {
+  const date = new Date()
+  if (month > 0) date.setMonth(month - 1)
+  if (year > 0) date.setFullYear(year)
+  return date
 }
-exports.showWeekly = showWeekly
 
-let calArray = () => {
-  let date = new Date()
+const createCalendarDays = date => {
+  date.setDate(1)
+  const spaces = new Array(date.getDay()).fill('  ')
   date.setMonth(date.getMonth() + 1)
   date.setDate(0)
-  const calArray = Array.from(new Array(date.getDate())).map((v, i) => i + 1)
-  return calArray
-}
-console.log(calArray())
+  const dayNumbers = [...Array(date.getDate()).keys()].map(i => ++i)
+  const days = spaces.concat(dayNumbers)
+  const calendarDays = days.map(i => ('  ' + i).slice(-2))
 
-exports.calArray = calArray
+  const weeks = [
+    calendarDays.slice(0, 7),
+    calendarDays.slice(7, 14),
+    calendarDays.slice(14, 21),
+    calendarDays.slice(21, 28),
+    calendarDays.slice(28, 35),
+    calendarDays.slice(35, 42)
+  ]
+  return weeks
+}
+
+const outputCalendar = (date) => {
+  let calendar = ''
+  const days = createCalendarDays(date)
+  days.forEach(function (item, weeknum) {
+    calendar = calendar.concat(days[weeknum].join(' '), '\n')
+  })
+  return calendar
+}
+
+main()
+
+exports.loadYearMonth = loadYearMonth
+exports.createCalendarDays = createCalendarDays
+exports.outputCalendar = outputCalendar
