b6f1dd9b111474f259bb987110128873efda9dc0
rosh-1228 <sho.watanabe1228@gmail.com>
2022-03-14 12:22:55 +0900

    DBのメモデータを削除するEraserクラスを作成

---
+++ 04.class/lib/eraser.js
@@ -0,0 +1,61 @@
+const { Select } = require('enquirer')
+
+class Eraser {
+  delete (id) {
+    const sqlite3 = require('sqlite3').verbose()
+    const path = require('path')
+    const db = new sqlite3.Database(path.join(__dirname, '..', 'db', ':memo:'))
+
+    db.serialize(() => {
+      const insertData = db.prepare('DELETE FROM memos WHERE id = (?)')
+      insertData.run([id])
+      insertData.finalize()
+    })
+    db.close()
+  }
+
+  select () {
+    this.load().then(contents =>
+      (async () => {
+        const prompt = new Select({
+          type: 'select',
+          name: 'memo',
+          value: 'memoId',
+          message: 'Choose a note you want to delete:',
+          choices: contents,
+          result (names) {
+            return this.map(names)
+          }
+        })
+        prompt.run()
+          .then(answer => this.delete(Object.values(answer).join()))
+      })()
+    )
+  }
+
+  async load () {
+    const sqlite3 = require('sqlite3').verbose()
+    const path = require('path')
+    const db = new sqlite3.Database(path.join(__dirname, '..', 'db', ':memo:'))
+
+    return new Promise((resolve, reject) => {
+      db.all(
+        'SELECT * FROM memos',
+        [],
+        (err, rows) => {
+          if (err) {
+            reject(err)
+          } else {
+            const contents = rows.map(row => {
+              console.log(row)
+              return { name: row.content.split(/\r\n|\r|\n/)[0], value: row.id }
+            })
+            resolve(contents)
+          }
+        }
+      )
+    })
+  }
+}
+
+module.exports = Eraser
