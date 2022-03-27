b791f90ef621b9bd696c0d91961c3fd0e27ae600
rosh-1228 <sho.watanabe1228@gmail.com>
2022-03-27 14:55:01 +0900

    関数readMessageをAdditionに移動、関数selectをexecuteに変更、addition.write()に引数を渡すように修正

--- 04.class/lib/memo.js
+++ 04.class/lib/memo.js
@@ -1,44 +1,31 @@
 #!/usr/bin/env node
 
-const View = require('./view')
+const Viewing = require('./viewing')
 const Addition = require('./addition')
 const Deletion = require('./deletion')
 const argv = require('minimist')(process.argv.slice(2))
+const sqlite3 = require('sqlite3').verbose()
+const path = require('path')
+const db = new sqlite3.Database(path.join(__dirname, '..', 'db', 'memo.db'))
 
 class Memo {
   constructor () {
-    this.view = new View()
-    this.deletion = new Deletion()
+    this.viewing = new Viewing(db)
+    this.deletion = new Deletion(db)
+    this.addition = new Addition(db)
   }
 
-  async readMessage (reader) {
-    return new Promise(resolve => {
-      const messages = []
-      reader.on('line', message => {
-        messages.push(message)
-        resolve(messages)
-      })
-    })
-  }
-
-  async select () {
+  async execute () {
     if ('l' in argv) {
-      this.view.show()
+      this.viewing.show()
     } else if ('r' in argv) {
-      this.view.select()
+      this.viewing.question()
     } else if ('d' in argv) {
-      this.deletion.select()
+      this.deletion.question()
     } else {
-      const reader = require('readline').createInterface({
-        input: process.stdin
-      })
-      const lines = await this.readMessage(reader)
-      process.stdin.on('end', function () {
-        const addition = new Addition(lines.join('\n'))
-        addition.write()
-      })
+      this.addition.write()
     }
   }
 }
 
-new Memo().select()
+new Memo().execute()
