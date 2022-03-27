b7b73efe2e7baabc2be42acb69c64ea165e2b106
rosh-1228 <sho.watanabe1228@gmail.com>
2022-03-27 15:26:57 +0900

    ファイル・DBの作成削除コードを削除、テストデータを削減

--- 04.class/test/test.js
+++ 04.class/test/test.js
@@ -1,130 +1,73 @@
-const chai = require('chai')
-const assert = chai.assert
+#!/usr/bin/env node
+/* eslint-env node, mocha */
+
+const assert = require('chai').assert
 const path = require('path')
-const View = require(path.join(__dirname, '..', 'lib', 'view'))
-const Addition = require(path.join(__dirname, '..', 'lib', 'addition'))
-
 const sqlite3 = require('sqlite3').verbose()
-const fs = require('fs')
-const Deletion = require('../lib/deletion')
-
+const db = new sqlite3.Database(path.join(__dirname, '..', 'db', 'memo.db'))
 const testData = [
-  { name: 'testData1', value: 'testData1' },
-  { name: 'testData2', value: 'testData2' },
-  { name: 'testData3', value: 'testData3' },
-  { name: 'testData4', value: 'testData4' },
-  { name: 'testData5', value: 'testData5' },
-  { name: 'testData6', value: 'testData6' },
-  { name: 'testData7', value: 'testData7' },
-  { name: 'testData8', value: 'testData8' },
-  { name: 'testData9', value: 'testData9' },
-  { name: 'testData10', value: 'testData10' }
+  { name: 'testData1', value: 'testData1' }
 ]
+const Viewing = require(path.join(__dirname, '..', 'lib', 'viewing'))
+const Addition = require(path.join(__dirname, '..', 'lib', 'addition'))
+const Deletion = require(path.join(__dirname, '..', 'lib', 'deletion'))
+const addition = new Addition(db)
 
-const testWritedData = [
-  { name: 'testData1', value: 'testData1' },
-  { name: 'testData2', value: 'testData2' },
-  { name: 'testData3', value: 'testData3' },
-  { name: 'testData4', value: 'testData4' },
-  { name: 'testData5', value: 'testData5' },
-  { name: 'testData6', value: 'testData6' },
-  { name: 'testData7', value: 'testData7' },
-  { name: 'testData8', value: 'testData8' },
-  { name: 'testData9', value: 'testData9' },
-  { name: 'testData10', value: 'testData10' },
-  { name: 'testPencilData', value: 'testPencilData' }
-]
-
-const testDeletedData = [
-  { name: 'testData2', value: 'testData2' },
-  { name: 'testData3', value: 'testData3' },
-  { name: 'testData4', value: 'testData4' },
-  { name: 'testData5', value: 'testData5' },
-  { name: 'testData6', value: 'testData6' },
-  { name: 'testData7', value: 'testData7' },
-  { name: 'testData8', value: 'testData8' },
-  { name: 'testData9', value: 'testData9' },
-  { name: 'testData10', value: 'testData10' },
-  { name: 'testPencilData', value: 'testPencilData' }
-]
-
-fs.mkdir(path.join(__dirname, '..', 'db'), (err) => {
-  if (err) { throw err }
-  fs.chmodSync(path.join(__dirname, '..', 'db'), '777', (err) => {
-    if (err) { throw err }
-  })
-})
-
-const db = new sqlite3.Database(path.join(__dirname, '..', 'db', ':memo:'), (err) => {
-  if (err) { throw err }
-  fs.chmodSync(path.join(__dirname, '..', 'db', ':memo:'), '777')
-})
-
-const loadMemo = () => {
-  return new Promise(resolve => {
-    const memo = new View().load()
-    resolve(memo)
-  })
+const loadMemo = async () => {
+  const db = new sqlite3.Database(path.join(__dirname, '..', 'db', 'memo.db'))
+  const viewing = new Viewing(db)
+  const memo = await viewing.load()
+  return memo
 }
 
-const writeMemo = (data) => {
+const addMemo = () => {
   return new Promise(resolve => {
-    new Addition(data).write()
+    addition.write()
     resolve()
   })
 }
 
-before(() => {
-  return new Promise((resolve) => {
-    db.serialize(() => {
-      db.run('CREATE TABLE memos (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT)')
-      for (let i = 1; i <= 10; i++) {
-        db.run('INSERT INTO memos(content) VALUES (?)', `testData${i}`)
+const deleteMemo = (id) => {
+  return new Promise(resolve => {
+    const db = new sqlite3.Database(path.join(__dirname, '..', 'db', 'memo.db'))
+    new Deletion(db).delete(id)
+    resolve()
+  })
+}
+
+const loadLastData = () => {
+  return new Promise((resolve, reject) => {
+    const db = new sqlite3.Database(path.join(__dirname, '..', 'db', 'memo.db'))
+    db.all('SELECT id FROM memos ORDER BY id DESC LIMIT 1', (err, resultid) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(resultid)
       }
-      db.run('SELECT 0', () => {
-        resolve()
-      })
     })
   })
+}
+
+before(async () => {
+  await addMemo()
 })
 
-describe('test memo app', () => {
-  it('test load memo', async () => {
+describe('test write memo', () => {
+  it('test write memo', async () => {
     const memo = await loadMemo()
     assert.deepEqual(memo, testData)
   })
 })
 
-describe('test writed memo', () => {
-  before(() => {
-    return new Promise((resolve) => {
-      new Addition('testPencilData').write()
-      resolve()
-    })
-  })
-
-  it('test write memo', async () => {
-    const memo = await loadMemo()
-    assert.deepEqual(memo, testWritedData)
-  })
-})
-
-describe('test deleted memo', () => {
-  before(() => {
-    return new Promise((resolve) => {
-      new Deletion().delete(1)
-      resolve()
-    })
+describe('test delete memo', () => {
+  before(async () => {
+    const result = await loadLastData()
+    await deleteMemo(result[0].id)
   })
 
   it('test delete memo', async () => {
     let memo = await loadMemo()
-    memo = await loadMemo()
-    assert.deepEqual(memo, testDeletedData)
+    memo = await loadMemo() // 最初のloadMemoより後にdeleteMemoが実行されるため、2回loadMemoを実行
+    assert.deepEqual(memo, [])
   })
-})
-
-after(() => {
-  db.close()
-  process.exit(0)
 })
